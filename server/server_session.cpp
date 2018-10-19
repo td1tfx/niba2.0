a98e712b8461e9fee950b0e0950f244ded7ddd6d
Felix Tian <felix.y.tian@gmail.com>
2018-10-19 01:16:25 -0400

    server ping
    client doesn't work yet as it is blocked by io

--- server/server_session.cpp
+++ server/server_session.cpp
@@ -13,7 +13,8 @@ using namespace nibaserver;
 
 server_session::server_session(boost::asio::io_context & ioc, 
     boost::asio::ip::tcp::socket && socket, boost::asio::ssl::context & ctx)
-    : ioc_(ioc), socket_(std::move(socket)), ws_(socket_, ctx)
+    : ioc_(ioc), socket_(std::move(socket)), ws_(socket_, ctx), timer_(socket_.get_executor().context(),
+    (std::chrono::steady_clock::time_point::max)())
 {
 }
 
@@ -23,6 +24,17 @@ void server_session::go()
     boost::asio::spawn(ioc_, [this, self](boost::asio::yield_context yield) {
         nibaserver::server_processor processor;
         try {
+            // start pinging
+            // auto ping
+            ws_.control_callback([this, self](boost::beast::websocket::frame_type kind,
+                boost::beast::string_view payload) {
+                boost::ignore_unused(kind, payload);
+                std::cout << "controlled ping" << std::endl;
+                ping_state_ = 0;
+                timer_.expires_after(std::chrono::seconds(15));
+            });
+            timer_.expires_after(std::chrono::seconds(15));
+            ping_timer({});
             ws_.next_layer().async_handshake(ssl::stream_base::server, yield);
             // Accept the websocket handshake
             ws_.async_accept_ex([](boost::beast::websocket::response_type& m)
@@ -30,15 +42,22 @@ void server_session::go()
                 // js-websocket(or chrome) require this field to be non-empty
                 m.insert(boost::beast::http::field::sec_websocket_protocol, "niba-server");
             }, yield);
+
             nibashared::request_dispatcher dispatcher(processor);
             for (;;)
             {
+                // recv request
                 std::string request_str;
                 auto buffer = boost::asio::dynamic_buffer(request_str);
                 ws_.async_read(buffer, yield);
-                std::string result = dispatcher.dispatch(request_str);
-                auto result_buffer = boost::asio::buffer(result);
-                ws_.async_write(result_buffer, yield);
+
+                // maintain as active
+                ping_state_ = 0;
+                timer_.expires_after(std::chrono::seconds(15));
+                
+                // process request and send out response
+                std::string response = dispatcher.dispatch(request_str);
+                ws_.async_write(boost::asio::buffer(response), yield);
             }
         }
         // unrecoverable error
@@ -48,5 +67,44 @@ void server_session::go()
             }
             std::cerr << "session ended " << e.what() << std::endl;
         }
+    });
+}
+
+void server_session::ping_timer(boost::system::error_code ec)
+{
+    // std::cout << "timer called " << (int)ping_state_ << std::endl;
+    if (ec && ec != boost::asio::error::operation_aborted)
+        return;
+
+    auto self(shared_from_this());
+
+    // See if the timer really expired since the deadline may have moved.
+    if (timer_.expiry() <= std::chrono::steady_clock::now())
+    {
+        // If this is the first time the timer expired,
+        // send a ping to see if the other end is there.
+        if (ws_.is_open() && ping_state_ == 0)
+        {
+            // Note that we are sending a ping
+            ping_state_ = 1;
+            // Set the timer
+            timer_.expires_after(std::chrono::seconds(15));
+            std::cout << "sending ping" << std::endl;
+            // Now send the ping
+            ws_.async_ping({}, [](boost::system::error_code ec) {
+                // std::cout << "ping sent" << std::endl;
+            });
+        }
+        else
+        {
+            std::cout << "connection closing" << std::endl;
+            ws_.next_layer().next_layer().cancel();
+            return;
+        }
+    }
+
+    // Wait on the timer
+    timer_.async_wait([this, self](boost::system::error_code ec) {
+        ping_timer(ec);
     });
 }
--- server/server_session.h
+++ server/server_session.h
@@ -5,6 +5,7 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/websocket.hpp>
 #include <boost/beast/websocket/ssl.hpp>
+#include <boost/asio/steady_timer.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/spawn.hpp>
 #include <boost/asio/ssl/stream.hpp>
@@ -20,9 +21,15 @@ public:
     ~server_session() = default;
     void go();
 private:
+    // error_code is required
+    void ping_timer(boost::system::error_code ec);
+
     boost::asio::io_context& ioc_;
     boost::asio::ip::tcp::socket socket_;
     boost::beast::websocket::stream<boost::asio::ssl::stream<boost::asio::ip::tcp::socket&>> ws_;
+    boost::asio::steady_timer timer_;
+
+    char ping_state_ = 0;
 };
 
 }
