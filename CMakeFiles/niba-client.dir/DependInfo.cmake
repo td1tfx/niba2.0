29f04d7961103884085f8f14d0bf0a15883858b8
tfx <td1tfx@163.com>
2018-11-22 20:08:06 +0900

    tested new data

---
+++ CMakeFiles/niba-client.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tfx/niba2.0/client/client_processor.cpp" "/home/tfx/niba2.0/CMakeFiles/niba-client.dir/client/client_processor.cpp.o"
+  "/home/tfx/niba2.0/client/client_session.cpp" "/home/tfx/niba2.0/CMakeFiles/niba-client.dir/client/client_session.cpp.o"
+  "/home/tfx/niba2.0/client/main.cpp" "/home/tfx/niba2.0/CMakeFiles/niba-client.dir/client/main.cpp.o"
+  "/home/tfx/niba2.0/shared/gamedata.cpp" "/home/tfx/niba2.0/CMakeFiles/niba-client.dir/shared/gamedata.cpp.o"
+  "/home/tfx/niba2.0/shared/message.cpp" "/home/tfx/niba2.0/CMakeFiles/niba-client.dir/shared/message.cpp.o"
+  "/home/tfx/niba2.0/shared/structs.cpp" "/home/tfx/niba2.0/CMakeFiles/niba-client.dir/shared/structs.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_COROUTINES_NO_DEPRECATION_WARNING"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "shared"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
