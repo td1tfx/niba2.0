4cfa8dd1d29657aeeff220235b9052e35ffce517
three0s <felix.y.tian@gmail.com>
2018-11-25 13:00:18 -0500

    allow unordered ids (#32)

--- shared/gamedata.cpp
+++ shared/gamedata.cpp
@@ -14,11 +14,15 @@ staticdata::staticdata() {
     try {
         std::ifstream char_fin("character.json");
         nlohmann::json serialized_chars = nlohmann::json::parse(char_fin);
-        characters_ = serialized_chars.get<decltype(characters_)>();
+        for (auto& element : serialized_chars) {
+            characters_[element["character_id"]] = element;
+        }
 
         std::ifstream magic_fin("magic.json");
         nlohmann::json serialized_magic = nlohmann::json::parse(magic_fin);
-        magics_ = serialized_magic.get<decltype(magics_)>();
+        for (auto& element : serialized_magic) {
+            magics_[element["magic_id"]] = element;
+        }
 
     } catch (std::exception &e) {
         // I don't care at this point, this should be tested statically
@@ -29,6 +33,6 @@ staticdata::staticdata() {
 
 // NOTE postgres index start at 1, and we may need to do some manually sequence
 // resetting if we get gaps (but that shouldn't be too hard)
-const character &staticdata::character(int id) { return characters_.at(id - 1); }
+const character &staticdata::character(int id) { return characters_.at(id); }
 
-const magic &staticdata::magic(int id) { return magics_.at(id - 1); }
+const magic &staticdata::magic(int id) { return magics_.at(id); }
