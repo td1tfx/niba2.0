e0998f8b4573a308b329ea396c4334ef0fa69e77
three0s <felix.y.tian@gmail.com>
2018-12-21 03:41:31 -0500

    server ping using coroutine

--- server/server_session.h
+++ server/server_session.h
@@ -13,6 +13,8 @@
 
 namespace nibaserver {
 
+
+
 class server_session : public std::enable_shared_from_this<server_session> {
 public:
     server_session(boost::asio::io_context &ioc, boost::asio::ip::tcp::socket &&socket,
@@ -21,15 +23,19 @@ public:
     void go();
 
 private:
-    // error_code is required
-    void ping_timer(boost::system::error_code ec);
+    enum class pingstate : char {
+        responsive = 0,
+        onhold = 1,
+    };
+    static constexpr int TIMEOUT = 15; // in seconds
 
     boost::asio::io_context &ioc_;
+    boost::asio::io_context::strand strand_;
     boost::asio::ip::tcp::socket socket_;
     boost::beast::websocket::stream<boost::asio::ssl::stream<boost::asio::ip::tcp::socket &>> ws_;
     boost::asio::steady_timer timer_;
     logger logger_;
-    char ping_state_ = 0;
+    pingstate ping_state_ = pingstate::responsive;
 };
 
 } // namespace nibaserver
